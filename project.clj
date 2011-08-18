59562e91dd12dae8f51a953d4d154fe8a876e7ec
Minh <minh.doboi@free.fr>
2011-08-19 01:04:47 +0200

    fix encoding + refac

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject hello-world "0.0.1"
   :dependencies
-    [[org.clojure/clojure "1.2.1"]
-     [ring/ring-jetty-adapter "0.3.9"]]
+  [[org.clojure/clojure "1.2.1"]
+   [org.clojure/clojure-contrib "1.2.0"]
+   [ring/ring-jetty-adapter "0.3.9"]]
 
   :aot [demo.web]
   :repl-init demo.web )
--- src/demo/web.clj
+++ src/demo/web.clj
@@ -1,27 +1,27 @@
 (ns demo.web
   (:use ring.adapter.jetty)
+  (:use clojure.contrib.monads)
   (:import (java.net InetAddress DatagramSocket DatagramPacket)))
 
 (def servers { "Waking_fields" ["94.23.229.44" 7778] })
 
 (defn usend [cmd ip port]
-      (let [ address (InetAddress/getByName ip)
-             socket  (new DatagramSocket)
-             buf     (.getBytes cmd)
-             packet  (new DatagramPacket buf (alength buf) address port) ]
-        (.send socket packet)
-        socket ))
+  (let [ address (InetAddress/getByName ip)
+         socket  (new DatagramSocket)
+         buf     (.getBytes cmd)
+         packet  (new DatagramPacket buf (alength buf) address port) ]
+    (.send socket packet)
+    socket ))
 
 (defn urecv [socket]
-      (let [ bufsize 1024
-             buf     (byte-array bufsize)
-             packet  (new DatagramPacket buf bufsize) ]
-        (doto socket
-          (.setSoTimeout 5000 )
-          (.setReceiveBufferSize bufsize)
-          (.receive packet))
-        (new String (.getData packet) 0 (.getLength packet)) ))
-
+  (let [ bufsize 1024
+         buf     (byte-array bufsize)
+         packet  (new DatagramPacket buf bufsize) ]
+    (doto socket
+      (.setSoTimeout 5000 )
+      (.setReceiveBufferSize bufsize)
+      (.receive packet))
+    (new String (.getData packet) 0 (.getLength packet) "UTF-8") ))
 
 (defn ask [[ip port]]
   (with-open [socket (usend "\\basic\\" ip port )]
@@ -30,19 +30,18 @@
 (defmacro link [target]
   `(str "<a href='/" ~target "'>" ~target "</a>"))
 
-(def links (for [k (keys servers)] (link k) ))
+(def homepage
+  (let [links (for [k (keys servers)] (link k) )]
+  {:status 200 :headers {"Content-Type" "text/html"} :body links }))
 
 (defn plainText [text]
   {:status 200 :headers {"Content-Type" "text/plain"} :body text})
 
 (defn app [req]
-  (if (= "/" (:uri req))
-    {:status 200 :headers {"Content-Type" "text/html"} :body links }
-    (let [ path (.substring (:uri req) 1 )
-           server (get servers path)]
-      (if (nil? server)
-        (plainText (str "Unknown " server))
-        (plainText (ask server))))))
+  (let [ path (.substring (:uri req) 1 )
+        server (get servers path)]
+    (with-monad maybe-m
+      (m-plus (m-fmap #(plainText(ask %)) server) homepage ))))
 
 
 (defn -main []
