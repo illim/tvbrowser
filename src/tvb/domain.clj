df17d1bbcf17b84a088452ab507260ea00842547
Minh <minh.doboi@free.fr>
2011-09-23 20:55:49 +0200

    refac

--- src/tvb/domain.clj
+++ src/tvb/domain.clj
@@ -4,63 +4,53 @@
   (:use tvb.utils))
 
 (defrecordx Player [name ping #^int score team])
-
-(defrecordx Team [name score])
+(defrecordx Team   [name score])
 
 (defrecord Admin [name email])
-
-(defrecord Game [map gametype team1 team2 players])
-
+(defrecord Game  [map gametype team1 team2 players])
 (defrecord Board [name admin game numplayers maxplayers password])
 
-(defn getPlayer [infos idx]
-  (xPlayer (map #(infos (str % "_" idx)) ["player" "ping" "score" "team"]) :coerce))
-
-(defn getTeam [infos idx]
-  (xTeam (map #(infos (str "team" idx %)) ["" "score"])))
 
 (defn getPlayers [infos team1 numplayers]
-  (letfn [(team-player [player]
+  (letfn [(getPlayer [idx] (xPlayer (map #(infos (str % "_" idx)) ["player" "ping" "score" "team"]) :coerce))
+          (team-player [player]
             [(= (:name team1) (:team player)) (:score player) (:name player)])]
     (loop [result (sorted-set-by (->> compare (by team-player) flip) []) idx numplayers]
       (if (zero? idx)
         result
-        (recur (conj result (getPlayer infos (- idx 1))) (dec idx))))))
+        (recur (conj result (getPlayer (- idx 1))) (dec idx))))))
 
 (defn toScoreBoard [{:strs [numplayers maxplayers password mapname gametype hostname adminname adminemail] :as infos}]
-  (let [nump          (Integer/parseInt numplayers)
-        [team1 team2] (map #(getTeam infos %) ["one" "two"])
-        players       (getPlayers infos team1 nump)]
-    (Board. hostname
-            (Admin. adminname adminemail)
-            (Game. mapname gametype team1 team2 players)
-            nump
-            (Integer/parseInt maxplayers)
-            (not= "0" password))))
+  (letfn [(getTeam [idx] (xTeam (map #(infos (str "team" idx %)) ["" "score"])))]
+    (let [nump          (Integer/parseInt numplayers)
+          [team1 team2] [(getTeam "one") (getTeam "two")]
+          players       (getPlayers infos team1 nump)]
+      (Board. hostname
+              (Admin. adminname adminemail)
+              (Game. mapname gametype team1 team2 players)
+              nump
+              (Integer/parseInt maxplayers)
+              (not= "0" password)))))
 
 
-(defn extractNumPlayers [message]
-  (Integer/parseInt (second (re-find #".*\\numplayers\\([^\\]*)\\.*" message) )))
-
-(defn toRegex [attributes]
-  (str "\\\\" (apply str (interpose "\\\\(.*)\\\\" attributes)) "\\\\(.*)\\\\final.*"))
-
+(defn extractNumPlayers [message]     (Integer/parseInt (second (re-find #".*\\numplayers\\([^\\]*)\\.*" message) )))
+(defn toRegex           [attributes]  (str "\\\\" (apply str (interpose "\\\\(.*)\\\\" attributes)) "\\\\(.*)\\\\final.*"))
+(def serverInfoPattern #"(.*):(.*) \\hostname\\(.*)\\numplayers\\(.*)\\maxplayers\\(.*)\\mapname\\(.*)")
 (def baseAttributes
    [ "mapname", "numplayers", "maxplayers", "hostname", "hostport", "gametype", "gamever", "password", "gamename", "gamemode", "teamone", "teamtwo", "teamonescore", "teamtwoscore", "adminname", "adminemail", "p","trackingstats", "dedicated" ])
 
-(defn playerAttributes [numPlayers]
-  (for [f (map #(fn [x] (str % "_" x)) ["player", "ping", "score", "team"])
-        n (range 0 numPlayers)]
-    (f n)))
 
 (defn parseScores [message]
-  (let [numPlayers (extractNumPlayers message)
-        attributes (into baseAttributes (playerAttributes numPlayers))
-        regex      (toRegex attributes)
-        infos      (rest (re-find (Pattern/compile regex) message)) ]
-    (zipmap attributes infos)))
+  (letfn [(playerAttributes [numPlayers]
+            (for [f (map #(fn [x] (str % "_" x)) ["player", "ping", "score", "team"])
+                  n (range 0 numPlayers)]
+              (f n)))]
+    (let [numPlayers (extractNumPlayers message)
+          attributes (into baseAttributes (playerAttributes numPlayers))
+          regex      (toRegex attributes)
+          infos      (rest (re-find (Pattern/compile regex) message)) ]
+      (zipmap attributes infos))))
 
-(def serverInfoPattern #"(.*):(.*) \\hostname\\(.*)\\numplayers\\(.*)\\maxplayers\\(.*)\\mapname\\(.*)")
 
 (defn parseServerInfo [message]
   (let [infos      (rest (re-find serverInfoPattern message))
